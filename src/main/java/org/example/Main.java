06bbcfa83c132dca2f8e3878215f6c8c62d23e52
cleone <jose_cleone@hotmail.com>
2023-05-05 18:43:23 -0300

    Subindo ajustes da aula 25

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,29 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Locale.setDefault(Locale.US);   // Esse cara serve para que o usuário digite usando o " . " o inves da " , ".
+        Scanner sc = new Scanner(System.in);  // Comando serve para ler/pegar o que o usuário está digitando.
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        char texto;
+        double quebrado;
+
+        System.out.println("digite um número quebrado: ");
+        quebrado = sc.nextDouble();
+
+
+        // essa parte --> %.2f%n, fala pro sistema que eu quero que ele mostre com duas casas decimais
+        // Lembrando que apra usar o --> %.2f%n tem de usar o printF e não o printLN
+        System.out.printf("Você digitou: %.2f%n", quebrado);
+
+
+        System.out.println("Digite um texto: ");
+        texto = sc.next().charAt(0);
+
+        System.out.println("A primeira letra do que você digitou é: " + texto);
     }
 }
